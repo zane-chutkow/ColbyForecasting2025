b9f28ff85dba38136cf035c2e7612424413fae36
Ben Tupper <btupper@bigelow.org>
2024-12-10 11:02:22 -0500

    Update C00_coding, add functions/examples.R

---
+++ functions/examples.R
@@ -0,0 +1,41 @@
+#' Read in a sunspot data
+#' 
+#' @return table of monthly sunspot activity
+#' @examples
+#' read_sunspots() |>
+#'  group_by(month) |>
+#'  group_modify(
+#'   function(tbl, key){
+#'     dplyr::tibble(
+#'     month = key$month,
+#'     min = min(tbl$sunspots),
+#'     q1 = quantile(tbl$sunspots, 0.25),
+#'     mean = mean(tbl$sunspots),
+#'     median = mean(tbl$sunspots),
+#'     q3 = quantile(tbl$sunspots, 0.75),
+#'     max = max(tbl$sunspots))
+#'   })
+read_sunspots = function(){
+  
+  s = start(sunspot.month)
+  s = sprintf("%0.4i-%0.2i-01", s[1], s[2]) |>
+    as.Date()
+  e = end(sunspot.month)
+  e = sprintf("%0.4i-%0.2i-01", e[1], e[2]) |>
+    as.Date()
+  date = seq(from = s, to = e, by = "month")
+  
+  dplyr::tibble(date = date,
+                month = format(date, "%b"),
+                month_factor = factor(month, levels = month.abb),
+                sunspots = as.vector(sunspot.month))
+}
+
+
+#' Read the Anderson iris data set
+#' 
+#' @return table of the famous iris data set
+read_iris = function(){
+  iris |>
+    dplyr::as_tibble()
+}
--- setup.R
+++ setup.R
@@ -7,25 +7,41 @@
 packages = list(
   CRAN = c("remotes", "dplyr", "ggplot2", "readr", "tidyr", "tidymodels", "sf", 
            "stars", "rnaturalearth", "robis"),
-  GITHUB = list(tidysdm = c(repo = "EvolEcolGroup/tidysdm", ref = "dev"))
+  GITHUB = NULL, #list(tidysdm = c(repo = "EvolEcolGroup/tidysdm", ref = "dev"))
+  RUNIV = list(tidysdm = c("https://evolecolgroup.r-universe.dev", 
+                             "https://cloud.r-project.org"))
 )
 
 # And here we check for prior installations and install locally as needed
 installed = installed.packages() |> rownames()
-ix = packages$CRAN %in% installed
-for (package in packages$CRAN[!ix]) {
-  install.packages(package)
+if ("CRAN" %in% names(packages)){
+  ix = packages$CRAN %in% installed
+  for (package in packages$CRAN[!ix]) {
+    install.packages(package)
+  }
 }
+
+if ("GITHUB" %in% names(packages)){
 ix = names(packages$GITHUB) %in% installed
-for (package in names(packages$GITHUB)[!ix]) {
-  remotes::install_github(packages$GITHUB[[package]]$repo,
-                          ref = packages$GITHUB[[package]]$ref)
+  for (package in names(packages$GITHUB)[!ix]) {
+    remotes::install_github(packages$GITHUB[[package]]$repo,
+                            ref = packages$GITHUB[[package]]$ref)
+  }
 }
+
+if ("RUNIV" %in% names(packages)){
+  ix = packages$CRAN %in% installed
+  for (package in names(packages$RUNIV[!ix])){
+    install.packages(package, repos = packages$RUNIV[[package]])
+  }
+}
+
 
 # Here we simply load each package form the library of packages
 suppressPackageStartupMessages({
   for (package in packages$CRAN) library(package, character.only = TRUE)
   for (package in names(packages$GITHUB)) library(package, character.only = TRUE)
+  for (package in names(packages$RUNIV)) library(package, character.only = TRUE, quietly = TRUE)
 })
 
 # Next we check the 'functions' directory for ".R" files and source those
@@ -33,9 +49,8 @@ for (f in list.files("functions", pattern = glob2rx("*.R"), full.names = TRUE)) 
   source(f, echo = FALSE)
 }
 
-# Finally set path to the data
+# Finally set path to the data m- hopefully as a sibling to the project directory
 # The data directory has top level subdirectories ("buoys", "coast", "brickman")
 # that contain data used by all.  It also may have one or more yearly directories
 # for "personal data directories" ala "2024", "2025"
-ROOT_DATA_PATH = "/mnt/s1/projects/ecocast/projects/ColbyForecasting/data"
-
+ROOT_DATA_PATH = "~/Dropbox/code/projects/ColbyForecasting_data/"
