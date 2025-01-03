a4120583a6c3ff03998b7169c46081eee9c6e282
Ben Tupper <btupper@bigelow.org>
2025-01-03 18:18:35 -0500

    populate C05_predictions

--- functions/predict.R
+++ functions/predict.R
@@ -1,9 +1,9 @@
 
-predict_model = function(model, newdata, type = "prob", threshold = 0.5, ...){
+predict_table = function(wflow, newdata, type = "prob", threshold = 0.5, ...){
   
-  #' Predict a classification model
+  #' Predict a classification from a table of new data
   #' 
-  #' @param model a model object
+  #' @param wflow a workflow object
   #' @param newdata table of new data including a `class` variable
   #' @param typ chr the type of prediction, by default "prob" - see `?predict.model_fit`
   #' @param threshold num the aribitray threshold used to define the outcome
@@ -11,8 +11,25 @@ predict_model = function(model, newdata, type = "prob", threshold = 0.5, ...){
   #' @return a table of predictions with the orginal class label
   
   lvls = levels(newdata$class)
-  predict(model, newdata, type = type, ...) |>
+  predict(wflow, newdata, type = type, ...) |>
     dplyr::mutate(.pred = if_else(.pred_presence >= threshold, lvls[1], lvls[2]) |>
                     factor(levels = lvls),
                   class = newdata$class)
+}
+
+
+predict_stars = function(wflow, newdata, type = "prob", threshold = 0.5, ...){
+  
+  #' Predict a classification from stars object
+  #' 
+  #' @param wflow a workflow object
+  #' @param newdata stars data
+  #' @param typ chr the type of prediction, by default "prob" - see `?predict.model_fit`
+  #' @param threshold num the aribitray threshold used to define the outcome
+  #' @param ... other arguments for `predict.model_fit`
+  #' @return a stars object
+  lvls = c("presence", "background")
+  predict(newdata, wflow, type = type, ...) |>
+    dplyr::mutate(.pred = if_else(.pred_presence >= threshold, lvls[1], lvls[2]) |>
+                    factor(levels = lvls))
 }
