e7dc3a31fe0af771b9d95d5c094b4b28b16b9f04
Zlchut27 <zchutkow@gmail.com>
2025-01-17 12:23:24 -0500

    Added code for Assignment 1

--- assignment_script_1.script.R
+++ assignment_script_1.script.R
@@ -2,7 +2,7 @@ DB = brickman_database() |>
   print()
 brickman_variables("all")
 
-db = DB |> dplyr::filter(interval == "static")
+db = DB |> dplyr::filter(scenario == "RCP45", year == "2055",interval == "mon")
 static_vars = read_brickman(db)
 
 
@@ -11,12 +11,15 @@ buoys = gom_buoys()
 
 buoy = filter(buoys,id == "M01")
 
-long_values = extract_brickman(x, buoys)
+long_values = extract_brickman(static_vars, buoys)
 
-longvals = filter(long_values,point == "p5")
+longvals = filter(long_values,point == "p5", name == "SST")
 
-short = select(long_values,month,value)
+longvals = longvals |>
+  mutate(month = factor(month, levels = month.abb))
 
-trial = filter(long_values, point == "p5")
 
-longvals
+
+ggplot(data = longvals,mapping = aes(x = month, y = value)) +
+  geom_point() + 
+  labs(title = "Temp Difference at Buoy M01")
