bb8e0616b538e6d06a45b714563371dac83c7a4b
Zlchut27 <zchutkow@gmail.com>
2025-01-17 15:59:12 -0500

    Observations.R Wrote code for a function to filter my data

--- functions/observations.R
+++ functions/observations.R
@@ -1,20 +1,23 @@
 
-read_observations = function(scientificname = "Mola mola",
-                             minimum_year = 1970, 
-                             ...){
+read_observations = function(scientificname = "Homarus americanus",
+                             minimum_year = 1970, basis = NULL, 
+                             minCount = NULL,maxCount = NULL
+                             ){
   
   #' Read raw OBIS data and then filter it
   #' 
   #' @param scientificname chr, the name of the species to read
   #' @param minimum_year num, the earliest year of observation to accept or 
   #'   set to NULL to skip
-  #' @param ... other arguments passed to `read_obis()`
+  #' @param basis, an optional argument to be used to select only a certain basis of record
+  #' @param minCount and maxCount, optional parameters to filter a range of individual counts to be included
+  #' @param other arguments passed to `read_obis()`
   #' @return a filtered table of observations
   
   # Happy coding!
   
   # read in the raw data
-  x = read_obis(scientificname, ...)
+  x = read_obis(scientificname)
   
   # if the user provided a non-NULL filter by year
   if (!is.null(minimum_year)){
@@ -22,5 +25,27 @@ read_observations = function(scientificname = "Mola mola",
       filter(year >= minimum_year)
   }
   
+  # if the user provided a non-Null filter by observation type
+  if(!is.null(basis)){
+    x = x |> filter(basisofRecord == basis)
+  }
+  x = x |> filter(!is.na(eventDate))
+  
+  if ((!is.null(minCount)) && (!is.null(maxCount)) && (minCount < maxCount)) {
+    x = x|> filter(individualCount > minCount, individualCount < maxCount)
+  }
+  
+  x = x |> filter(!is.na(individualCount))
+  
+  db = brickman_database() |>
+    filter(scenario == "STATIC", var == "mask")
+  mask = read_brickman(db, add_depth = FALSE)
+  hitOrMiss = extract_brickman(mask, x)
+  
+  x = x |> filter(!is.na(hitOrMiss$value))
+  
+  
   return(x)
 }
+
+  
